f7cdc777dd4c3e9b7623ebeb80e62d31e6ee70a8
Daniel Alejandro Gaona Mercado <daniel.gaona@pjedomex.gob.mx>
2023-02-28 22:55:32 -0600

    se restructura el proyecto

--- metro.config.js
+++
@@ -1,4 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json']
-module.exports = getDefaultConfig(__dirname);
