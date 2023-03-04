2ecf39e171e1fd642d0aa0a7a1e15fb2082f1d7b
Daniel Alejandro Gaona Mercado <67657380+dgaonam@users.noreply.github.com>
2023-03-04 11:24:56 -0600

    Update and rename config/firebase.jsx to configuración/firebase.jsx

---
+++ configuración/firebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHZ6RHvEuaYfUG9JXjIRgWK-5cEnxfEQ8",
+  authDomain: "curso-f876a.firebaseapp.com",
+  databaseURL: "https://curso88-f876a-defautt-rtdb.firebaseio.com",
+  projectId: "curso88-f756a",
+  storageBucket: "curso88-f876a.appspot.com",
+  messagingSenderId: "143594891960",
+  appId: "1:143594891060:web:2d7531aa1a4a3fc64e5761",
+  measurementId: "G-X3WK684BT4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
